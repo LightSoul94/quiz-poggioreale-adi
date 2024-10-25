fdc7ccfd896987cd31945ccbe12cf5c182e2f5ba
Gianluca <g.caliendo@soluzioniinformatiche.info>
2024-10-25 17:46:37 +0200

    - Creata pagina per quiz

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,17 +1,10 @@
-import { getQuestionsFromFirestore } from '../models/questionModel.js';
-
 const loadQuestions = async (req, res) => {
     const nickname = req.body.nickname;
     if (!nickname) {
         return res.redirect('/');
     }
 
-    try {
-        const questions = await getQuestionsFromFirestore();
-        res.render('lobby', { nickname, questions });
-    } catch (error) {
-        res.status(500).send('Errore nel caricamento delle domande');
-    }
+    res.render('lobby', { nickname });
 };
 
 export default { loadQuestions };
